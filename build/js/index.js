d833d93cd34e4f8a101783a4080940d1a98dfb61
ilija_nikolaev <nikolaev.xo@gmail.com>
2020-07-12 12:47:48 +0400

    init project

---
+++ build/js/index.js
@@ -0,0 +1,59 @@
+var team = new Swiper('.swiper-team', {
+    slidesPerView: 5,
+    spaceBetween:20,
+    lazy: true,
+    loop:true,
+    navigation: {
+        nextEl: '.team__arrows-block .button-next',
+        prevEl: '.team__arrows-block .button-prev',
+    },
+
+});
+var documents = new Swiper('.swiper-documents', {
+    slidesPerView: 3,
+    spaceBetween:20,
+    lazy: true,
+    loop:true,
+    navigation: {
+        nextEl: '.documents__arrows-block .button-next',
+        prevEl: '.documents__arrows-block .button-prev',
+    },
+
+});
+
+var accreditadion = new Swiper('.swiper-accreditadion', {
+    slidesPerView: 3,
+    spaceBetween:20,
+    lazy: true,
+    loop:true,
+    navigation: {
+        nextEl: '.swiper-accreditadion .button-next',
+        prevEl: '.swiper-accreditadion .button-prev',
+    },
+
+});
+team.on('slideChangeTransitionEnd',function () {
+    let slideCite = ($(".swiper-slide-active").find(".team__slide").data("cite"));
+    let citeBlock = $(".js-cite-block");
+    citeBlock.text(slideCite)
+});
+
+var popups = {
+    popup:$(".js-callback"),
+    menu:$(".js-menu"),
+    popupImg:$(".popup__background svg"),
+    popupToggle:function (element) {
+        element.fadeToggle(300);
+        this.popupImg.toggleClass("opened")
+    },
+    popupToggler:$(".js-popup-toggler"),
+    menuToggler:$(".js-menu-toggler"),
+}
+
+popups.popupToggler.on("click",function () {
+    popups.popupToggle(popups.popup)
+})
+
+popups.menuToggler.on("click",function () {
+    popups.popupToggle(popups.menu)
+})
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var sourcemaps = require('gulp-sourcemaps');
+var server = require('browser-sync').create();
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var imagemin = require("gulp-imagemin");
+
+sass.compiler = require("node-sass");
+
+
+gulp.task("html", function () {
+    return gulp.src("source/*.html ")
+         .pipe(posthtml([ include() ]))
+         .pipe(gulp.dest("build"));
+      });
+
+
+gulp.task("images", function () {
+    return gulp.src("source/images/**/*.{png,jpg,svg}")
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.mozjpeg({progressive: true}),
+            imagemin.svgo()
+        ]))
+        .pipe(gulp.dest("build/images"));
+});
+
+gulp.task("style",function () {
+     return gulp.src("source/scss/main.scss")
+         .pipe(sourcemaps.init())
+         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+         .pipe(sourcemaps.write('/maps'))
+         .pipe(gulp.dest("build/css/"))
+         .pipe(server.stream());});
+
+gulp.task("dev",  function() {
+
+     server.init({
+          server: "build"
+     });
+     gulp.watch("source/scss/main.scss", gulp.series('style'));
+     gulp.watch("source/*.html", gulp.series('html'));
+     gulp.watch("source/*.html").on('change', server.reload);
+     gulp.watch("build/**/*.js").on('change', server.reload);
+
+});
+
+
+
